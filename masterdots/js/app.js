c2820ed4a74c59ef8b163fcd996f3e3700575566
pacogomezarnal <paco.gomez.arnal@gmail.com>
2022-09-06 13:08:47 +0200

    Mensaje de error HTML

--- masterdots/js/app.js
+++ masterdots/js/app.js
@@ -4,14 +4,19 @@ COMPROBACION DE FORMULARIO
 function comprobar(event){
     //Comprobación campo nick
     if(nick.value.length==0){
+        event.preventDefault();
         //Ponemos el foco en la caja del nick
         nick.focus();
+        //Escribimos el error
+        error.innerText="El campo nick no puede estar vacío";
         //Salimos
-        event.preventDefault();
         return false;
     }else if(tamano.value=="0"){
+        event.preventDefault();
         //Ponemos el foco en la caja del nick
         tamano.focus();
+        //Escribimos el error
+        error.innerText="Se debe seleccionar un tamaño para nuestro panel";
         //Salimos
         event.preventDefault();
         return false;
@@ -23,4 +28,5 @@ function comprobar(event){
 const form_entrada = document.getElementById("form_entrada");
 const nick = document.getElementById("nick");
 const tamano = document.getElementById("tamano");
+const error = document.getElementById("error");
 form_entrada.addEventListener('submit', comprobar);
