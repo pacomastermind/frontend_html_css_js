34c9a624bce918261d47fb5e02eab92d725102d4
pacogomezarnal <paco.gomez.arnal@gmail.com>
2022-09-08 17:36:47 +0200

    Añadidas las dinámicas del juego

--- masterdots/js/juego.js
+++ masterdots/js/juego.js
@@ -3,8 +3,63 @@ JS PÁGINA JUEGO
 
 STORAGE
 */
+
+//Variables necesarias para la dinámica del juego
+var comienzaMarcar=false;
+var classMarcada="";
+var idMarcado=-1
+var adyacentes=[];
+//PROPONER ESTE ERROR
+//var tamano = parseInt(tamanoSession);
+var tamano = -1;
+
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
+}
+
+function calcularAdyacentes(){
+    adyacentes=[];
+    //Calculamos todos los adyacentes
+    //Las excepciones son la primera y última columna, ya que
+    //los adyacentes izquierda y/o derecha no son válidos
+    //Adyacente izquierda, columna 1(números que al dividir por tamano el resto es 0) excepcion
+    if(((idMarcado-1)>=0)&&((idMarcado%tamano)>0)) adyacentes.push(idMarcado-1)
+    //Adyacente arriba
+    if((idMarcado-tamano)>=0) adyacentes.push(idMarcado-tamano)
+    //Adyacente derecha, ultima columna (números+1 que al dividir por tamano el resto es 0) excepcion
+    if(((idMarcado+1)<(tamano*tamano))&&(((idMarcado+1)%tamano)>0)) adyacentes.push(idMarcado+1)
+    //Por ultimo adyacente abajo
+    if((idMarcado+tamano)<(tamano*tamano)) adyacentes.push(idMarcado+tamano)
+
+}
+
+function comenzarMarcar(event){
+    if(!comienzaMarcar) comienzaMarcar=true;
+    if(event.target.classList.contains('rojo')){
+        event.target.parentElement.classList.add('rojo');
+        classMarcada='rojo';
+    }
+    else{
+        event.target.parentElement.classList.add('verde');
+        classMarcada='verde';
+    }
+    //Calculamos adyacentes
+    idMarcado=parseInt(event.target.id);
+    calcularAdyacentes();
+}
+function continuaMarcando(event){
+    //Solo se activa si se ha comenzado a marcar con un elemento
+    if(comienzaMarcar){
+        //Solo se podrá marcar si el item sobre el que se está pasando por encima 
+        //es un adyacente y es del mismo color
+        let idNuevoMarcado=parseInt(event.target.id);
+        if(adyacentes.includes(idNuevoMarcado)&&event.target.classList.contains(classMarcada)){
+            //Un nuevo elemento se ha unido
+            idMarcado=parseInt(event.target.id);
+            event.target.parentElement.classList.add(classMarcada);
+            calcularAdyacentes();
+        }
+    }
 }
 
 //Capturamos los datos de la sesión y colocamos nick y avatar
@@ -20,13 +75,26 @@ document.getElementById("juego").style.gridTemplateRows="repeat("+tamanoSession+
 let items="";
 let color=["rojo","verde"];
 let colorRnd=0;
-for (let index = 0; index < parseInt(tamanoSession)*parseInt(tamanoSession); index++) {
+tamano=parseInt(tamanoSession);
+for (let index = 0; index < tamano*tamano; index++) {
     if(index%2>0){
         colorRnd=getRandomInt(2);
     }
-    items+=`<div class="containerItem"><div class="item ${color[colorRnd]}"></div></div>`;
+    items+=`<div class="containerItem"><div id="${index}" class="item ${color[colorRnd]}"></div></div>`;
 }
 document.getElementById('juego').innerHTML=items;
+//Añadimos el evento mousedown sobre todos los containerItem
+const containerItems=document.getElementsByClassName('item');
+for (let item of containerItems) {
+    item.addEventListener('mousedown',comenzarMarcar);
+    item.addEventListener('mouseover',continuaMarcando);
+}
+//Reseteare cuando suelte el raton
+document.addEventListener('mouseup',(event)=>{
+    comienzaMarcar=false;
+    idMarcado=-1;
+    adyacentes=[];
+})
 //Comprobamos que los datos no sean nulos
 let comprobarDatos=checkDatosUsuario();
 if(!comprobarDatos) location="index.html";
